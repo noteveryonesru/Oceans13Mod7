76e589043921edca092049421c915906cb95b2bc
Ariel <arieldoria15@gmail.com>
2020-08-25 16:18:23 +0800

    Resolve the conflict in index.js

--- OceansDatabase/index.js
+++ OceansDatabase/index.js
@@ -4,18 +4,11 @@ var cors = require('cors')
 
 const app = express();
 
-<<<<<<< HEAD
-//app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-//app.use(bodyParser.urlencoded({extended:false}));
-=======
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
->>>>>>> 28e8c50ce52a586df1a2b30e0f30301f740f455b
 app.use(cors())
 const router = require('./router')
 router(app)
